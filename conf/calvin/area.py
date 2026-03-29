04a3f195471493160b19480d349a3d101bedbfb0
JRGruhnert <36522183+JRGruhnert@users.noreply.github.com>
2026-03-29 16:37:24 +0200

    tempkkkdhj

--- conf/calvin/area.py
+++ conf/calvin/area.py
@@ -1,6 +1,17 @@
 from dataclasses import dataclass
 
+from src.states.logic.addons.addon_euler import EulerTapasAddonConfig
 from src.states.logic.area.area import AreaConfig
+
+from src.states.logic.addon import AddonConfig
+from src.states.logic.area.area_value_cnd import AreaValueConditionConfig
+from src.states.logic.boundary import BoundaryConfig
+from src.states.logic.eval_cnd import EvalConditionConfig
+from src.states.logic.location.euclidean_distance_cnd import (
+    EuclideanDistanceConditionConfig,
+)
+from src.states.logic.value_cnd import ValueConditionConfig
+from src.states.state import StateConfig
 
 
 @dataclass
@@ -23,4 +34,21 @@ class CalvinAreaConfig(AreaConfig):
         "table": [[-0.02, -0.17, 0.44], [0.32, -0.01, 0.54]],
         "drawer_open": [[0.02, -0.37, 0.34], [0.32, -0.23, 0.44]],
         "drawer_closed": [[0.02, -0.18, 0.34], [0.32, -0.00, 0.44]],
+    }
+
+
+@dataclass
+class CalvinAreaStateConfig(StateConfig):
+    type_str: str = "AreaEuler"
+    size: int = 6
+    value_cnd = AreaValueConditionConfig(
+        area=CalvinAreaConfig(),
+        boundary=BoundaryConfig(),
+    )
+    distance_cnd_skill = EuclideanDistanceConditionConfig()
+    distance_cnd_goal = EuclideanDistanceConditionConfig()
+    eval_cnd = EvalConditionConfig()
+    value_cnd_eval: ValueConditionConfig | None = None
+    addons = {
+        "tapas": EulerTapasAddonConfig(),
     }
